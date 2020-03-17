2bed65182253e55fea74c79b5be7b0a8fe3e2d6e
Jeff Webb <webbwear@icloud.com>
2020-03-17 10:00:23 -0400

    Streams page api call for data

--- src/components/Streams.js
+++ src/components/Streams.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../api';
 
 function Stream() {
+    const [channels, setChannels] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await api.get('https://api.twitch.tv/helix/streams')
+            let dataArray = result.data.data 
+            console.log(dataArray)
+        }
+        fetchData()
+    })
     return <div>streams component</div>
 }
 
