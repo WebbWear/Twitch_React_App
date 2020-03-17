ad97e4b925cd0cda416b9a763b986a24b44568dd
Jeff Webb <webbwear@icloud.com>
2020-03-17 09:39:37 -0400

    Added gamestream perams with id + how many viewing

--- src/components/Games.js
+++ src/components/Games.js
@@ -11,19 +11,22 @@ function Games() {
             // console.log(result.data);
             let dataArray = result.data.data
             let finalArray = dataArray.map(game => {
-            let newURL = game.box_art_url.replace('{width}', '300').replace('{height}', '300');
-                game.box_art_url = newURL
+            let newURL = game.box_art_url
+            .replace('{width}', '300')
+            .replace('{height}', '300');
+                game.box_art_url = newURL;
+                return game
             })
-            setGames(result.data.data);
+            setGames(finalArray);
         };
         fetchData();
-    });
+    }, []);
     return (
         <div>
             <h1>Most Popular Games</h1>
             <div className='row'>
                 {games.map(game => (
-                    <div className='col-4'>
+                    <div className='col-lg-4 col-md-6 col-sm-12 mt-5'>
                         <div className="card">
                             <img className="card-img-top" src={game.box_art_url} alt="Games" />
                             <div className="card-body">
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Games from './components/Games';
 import Header from './components/Header';
 import Stream from './components/Streams';
+import GameStreams from './components/GameStreams';
+
 import ReactDOM from "react-dom";
 
 import "./styles.css";
@@ -15,6 +17,8 @@ function App() {
         <Header />
         <Route exact path='/' component={Games} />
         <Route exact path='/top-streams' component={Stream} />
+        <Route exact path='/game/:id' component={GameStreams} />
+
 
         </Router>
     );
