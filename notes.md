c216e0761e1d6943b1d329fee2c01628813c0a44
Jeff Webb <webbwear@icloud.com>
2020-03-20 11:55:42 -0400

    Modified index page removed Link not needed

---
+++ notes.md
@@ -0,0 +1,8 @@
+Made this from tutorial on youtube account "Renaissance Troll":
+https://www.youtube.com/watch?v=VTY6ZzDTV3A
+and  second video
+https://www.youtube.com/watch?v=4FqIvnW0l5s
+
+Created a account on Twitch.tv and used the docs at:
+https://dev.twitch.tv/docs/api
+
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Games from './components/Games';
 import Header from './components/Header';
 import Stream from './components/Streams';
